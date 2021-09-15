b59ef28fa280f995e3e267670cd1f4932ba0bc2b
mike <michael.hallam3@gmail.com>
2021-09-15 13:43:58 +0100

    added more traditional test data

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import cv2 as cv
 import analyser
 
+
 def test_img():
     import os
-    fname = 'test_lat_flow.jpg'
+    fname = 'test_lat_flow_3.jpg'
     if os.path.exists(fname):
-        return cv.imread(fname)
+        return cv.imread(fname)[150:1550, 450:900]
     else:
         raise FileNotFoundError('no')
 
 
 if __name__ == '__main__':
-    img = test_img()
-    f = lambda: [img]
+    f = lambda: [test_img()]
     analysed = analyser.analyse(f, debug=False)
     print(analysed)
 
